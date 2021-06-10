ec9fde7d889d7e7fe42956fd64d1b4630a3b961b
github_username <you@example.com>
2021-06-10 20:00:10 +0300

    first commit

---
+++ kiosk.js
@@ -0,0 +1,30 @@
+/*document.body.style.backgroundColor= "silver";
+document.getElementById("title").style.color="green";
+var x = document.getElementsByTagName("h3");
+
+for (i= 0;i<x.length;i++){
+    x[i].style.textTransform="uppercase";
+}
+
+var node = document.createElement("li");                 
+var textnode = document.createTextNode("passion");        
+node.appendChild(textnode);                             
+document.getElementById("fruList").appendChild(node);  
+
+var node = document.createElement("li");                 
+var textnode = document.createTextNode("spinach");        
+node.appendChild(textnode);                             
+document.getElementById("vegList").appendChild(node);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
